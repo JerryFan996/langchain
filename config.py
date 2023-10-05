a6a04b75772f10cdbc4b94d8a4a280f0a793ec8b
JerryFan996 <88005345+JerryFan996@users.noreply.github.com>
2023-10-06 04:46:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # This will store any configuration variables for the application
 
-OPENAI_API_KEY = "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"
-GOOGLE_API_KEY = "AIzaSyCxmBfUPCYydxamNkI9IUqU2BHnXX1S9HI"
-GOOGLE_CSE_ID = "c68261e87b7e0498b"
+OPENAI_API_KEY = 
+GOOGLE_API_KEY = 
+GOOGLE_CSE_ID = 
